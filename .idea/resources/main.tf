27021e05defd3df4570a423c8bea67d5f99b0390
devops-aws87 <tamil.vec@gmail.com>
2025-12-02 09:49:22 -0500

    commit_v1

---
+++ .idea/resources/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+      "sg-056bd46848ffb08a9"
+    ]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_route53_record" "www" {
+    zone_id= "Z018624414H1YR1TZOP35"
+    name = "test"
+    type = "A"
+    ttl = "300"
+    records = [aws_instance.frontend.private_ip]
+    }
