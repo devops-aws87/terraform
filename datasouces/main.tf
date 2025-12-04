ed1a76731de7594d8ccdc0c0499a4255b2cdde5e
devops-aws87 <tamil.vec@gmail.com>
2025-12-04 16:26:39 -0500

    adding data source

---
+++ datasouces/main.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "example" {
+
+  filter {
+    name = "name"
+    values = ["ami-09c813fb71547fc4f"]
+  }
+  owners = [973714476881]
+}
+
+output "amioutput" {
+  value = "data.aws_ami.example"
+}
